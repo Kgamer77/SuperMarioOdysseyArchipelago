c6df7bf59e9a42cda3931a8bb7f957808be9fa2e
Kgamer77 <147015128+Kgamer77@users.noreply.github.com>
2025-04-07 22:15:51 -0700

    Removed Unneeded Files
    Removal of files added from gitignore issue

--- Mod/build100/crt0.d
+++
@@ -1,2 +0,0 @@
-crt0.o: \
- /mnt/c/Users/Kevin/source/repos/SuperMarioOdysseyArchipelago/Mod/source/crt0.s
--- Mod/build100/module.d
+++
@@ -1,2 +0,0 @@
-module.o: \
- /mnt/c/Users/Kevin/source/repos/SuperMarioOdysseyArchipelago/Mod/source/module.cpp
